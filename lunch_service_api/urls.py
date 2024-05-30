3d429d2da5a8a47a35d26e861abcfa946cdd1772
pnasonov <pavlonasonov@gmail.com>
2024-05-30 17:16:20 +0300

    route urls for employee

--- lunch_service_api/urls.py
+++ lunch_service_api/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/lunch/", include("restaurant.urls", namespace="restaurant")),
+    path("api/", include("restaurant.urls", namespace="restaurant")),
 ]
--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,10 +1,11 @@
 from rest_framework import routers
 
-from restaurant.views import RestaurantViewSet, MenuViewSet
+from restaurant.views import RestaurantViewSet, MenuViewSet, EmployeeViewSet
 
 router = routers.DefaultRouter()
 router.register("restaurants", RestaurantViewSet)
 router.register("menus", MenuViewSet)
+router.register("employees", EmployeeViewSet)
 
 
 urlpatterns = router.urls
