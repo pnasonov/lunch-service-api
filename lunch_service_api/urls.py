e75d131d78c99ebd29d8c5a5d76eb366672fcf43
pnasonov <pavlonasonov@gmail.com>
2024-05-30 20:34:10 +0300

    install JWT and set up user and point and email features

--- lunch_service_api/urls.py
+++ lunch_service_api/urls.py
@@ -21,4 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("restaurant.urls", namespace="restaurant")),
+    path("api/user/", include("user.urls", namespace="user")),
 ]
