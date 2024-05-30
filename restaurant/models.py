7c8e20c6c8a024850ff80ffe78785830ed88a734
pnasonov <pavlonasonov@gmail.com>
2024-05-30 13:48:36 +0300

    added models and migrations for restaurants

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Menu(models.Model):
+    date_upload = models.DateField(auto_now_add=True, unique_for_date=True)
+    description = models.TextField()
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ("date_upload",)
+        unique_together = ("date_upload", "restaurant")
+
+    def __str__(self) -> str:
+        return f"{self.restaurant.name} - {self.date_upload}"
